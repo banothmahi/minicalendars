ecadef4b4c69b4eca8c77ad0d5eeb9c6f666217b
banothmahi <162912462+banothmahi@users.noreply.github.com>
2025-07-21 07:24:33 +0530

    Add files via upload

---
+++ webpack.config.base.js
@@ -0,0 +1,32 @@
+var path = require( 'path' );
+var webpack = require( 'webpack' );
+
+module.exports = {
+    context: __dirname,
+    entry: './src/index',
+    output: {
+        path: path.resolve( './lib/' ),
+        libraryTarget: 'umd',
+        filename: 'index.js'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                loaders: [ 'babel' ]
+            },
+            {
+                test: /.css$/,
+                loaders: [
+                    'style',
+                    'css?modules&importLoaders=1&localIdentName=[path]__[name]__[local]__[hash:base64:5]'
+                ]
+            }
+        ]
+    },
+    resolve: {
+        modulesDirectories: [ 'node_modules' ],
+        extensions: [ '', '.js', '.jsx' ]
+    }
+};
